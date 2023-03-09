cab6bffd3bcc05a5ecb0784600fa5f2d8f357bec
JohnDGC <jonchygomez@yahoo.es>
2023-03-08 19:16:51 -0500

    cambios en el pipe find

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -9,16 +9,20 @@ export class FindPipe implements PipeTransform {
 
   transform(tasks: TodoDto[], arg: string): TodoDto[] {
 
-    if(!arg || arg?.length < 3) return tasks;
-    let result: any[] = [];
+    if (!arg || arg?.length < 1) return tasks;
+    // let result: any[] = [];
+    let tempArr;
 
-    for (const value of tasks) {
-      if(value.description.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
-        result = [...result, value];
-        console.log(result)
-        // tasks.filter
-      }
-    }
-    return result;
+      tempArr = tasks.filter((task) => task.description.toLowerCase().indexOf(arg.toLowerCase()) > -1)
+      // tempArr = tasks.filter((task) => task.description.toLowerCase().includes(arg.toLowerCase()))
+      // result = tempArr;
+
+    // for (const value of tasks) {
+    //   if(value.description.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
+    //     result = [...result, value];
+    //     console.log(result)
+    //   }
+    // }
+    return tempArr;
   }
 }
